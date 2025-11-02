dffc2fb566821606d4f4358144934e9273e6a6c1
Sashriya <sashriya.murali@gmail.com>
2025-11-02 17:35:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Todo from './components/todo'
+import Todo from './components/todo';
 
 function App() {
   return (
@@ -9,4 +9,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
